c39837b6c09eb6c066653d21e1e911bc2c43f5bf
tomzcn123 <79131150+tomzcn123@users.noreply.github.com>
2023-04-22 15:27:01 -0400

    Update stocks_app.py

--- stocks_app.py
+++ stocks_app.py
@@ -12,37 +12,35 @@ def get_sp500_tickers():
     return tickers
 
 
-def calculate_macd(stock_ticker, period='20d', interval='1d'):
+def calculate_moving_average(stock_ticker, period='20d', interval='1d', window=20):
     data = yf.download(tickers=stock_ticker, period=period, interval=interval)
-    macd_indicator = MACD(data['Close'], window_slow=26, window_fast=12, window_sign=9)
-    data['MACD'] = macd_indicator.macd()
+    data['MovingAverage'] = data['Close'].rolling(window=window).mean()
     return data
 
-
-
-def find_stocks_above_macd(stock_list):
-    stocks_above_macd = []
+def find_stocks_above_moving_average(stock_list):
+    stocks_above_moving_average = []
 
     for stock in stock_list:
         try:
-            data = calculate_macd(stock)
+            data = calculate_moving_average(stock)
 
-            if not data.empty and data.iloc[-1]['Close'] > data.iloc[-1]['MACD']:
-                stocks_above_macd.append(stock)
+            if not data.empty and data.iloc[-1]['Close'] > data.iloc[-1]['MovingAverage']:
+                stocks_above_moving_average.append(stock)
         except Exception as e:
             st.warning(f"Error processing stock {stock}: {e}")
 
-    return stocks_above_macd
+    return stocks_above_moving_average
 
 
 
-st.title("Stocks with the Current Price Above the Daily 20 MACD")
+
+st.title("Stocks with the Current Price Above the 20-Day Moving Average")
 
 st.write("Fetching S&P 500 stock tickers...")
 sp500_tickers = get_sp500_tickers()
 
 st.write("Analyzing stocks...")
-stocks_above_macd = find_stocks_above_macd(sp500_tickers)
+stocks_above_moving_average = find_stocks_above_moving_average(sp500_tickers)
 
-st.header("Stocks with the current price above the daily 20 MACD:")
-st.write(stocks_above_macd)
+st.header("Stocks with the current price above the 20-day moving average:")
+st.write(stocks_above_moving_average)
