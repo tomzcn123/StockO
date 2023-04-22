ce40e705616a8fc80aa72b250a6487a450948e04
tomzcn123 <79131150+tomzcn123@users.noreply.github.com>
2023-04-22 15:53:14 -0400

    Update stocks_app.py

--- stocks_app.py
+++ stocks_app.py
@@ -8,8 +8,9 @@ import lxml
 def get_sp500_tickers():
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     table = pd.read_html(url, header=0)[0]
-    tickers = table['Symbol'].tolist()
+    tickers = table[['Symbol', 'GICS Sector']].to_dict('records')
     return tickers
+
 
 def calculate_moving_average(stock_ticker, period='100d', interval='1d', window=20):
     data = yf.download(tickers=stock_ticker, period=period, interval=interval)
@@ -24,9 +25,11 @@ def calculate_macd(data, window_fast=12, window_slow=26, window_sign=9, macd_ma_
 
 
 def find_stocks_above_conditions(stock_list):
-    stocks_above_conditions = []
+    stocks_above_conditions = defaultdict(list)
 
-    for stock in stock_list:
+    for stock_info in stock_list:
+        stock = stock_info['Symbol']
+        sector = stock_info['GICS Sector']
         try:
             data = calculate_moving_average(stock)
             data = calculate_macd(data, window_fast=5, macd_ma_window=5)
@@ -36,8 +39,7 @@ def find_stocks_above_conditions(stock_list):
                 data.iloc[-1]['Close'] > data.iloc[-1]['MovingAverage'] and
                 data.iloc[-1][f'MACD_5_26_9_MA_5'] > data.iloc[-1]['MACD_5_26_9']
             ):
-                stocks_above_conditions.append(stock)
-                
+                stocks_above_conditions[sector].append(stock)
         except Exception as e:
             st.warning(f"Error processing stock {stock}: {e}")
 
@@ -49,12 +51,14 @@ def find_stocks_above_conditions(stock_list):
 
 st.title("Stocks with the Current Price Above the 20-Day Moving Average and 5-Day MACD Line")
 
-st.write("Fetching S&P 500 stock tickers...")
+st.write("Fetching S&P 500 stock tickers and sectors...")
 sp500_tickers = get_sp500_tickers()
 
 st.write("Analyzing stocks...")
 stocks_above_conditions = find_stocks_above_conditions(sp500_tickers)
 
 st.header("Stocks with the current price above the 20-day moving average and 5-day MACD line:")
-st.write(stocks_above_conditions)
+for sector, stocks in stocks_above_conditions.items():
+    st.subheader(sector)
+    st.write(stocks)
 
