ecfe308e9a3b0deee96cb384de2d0a0ba5a9908a
tomzcn123 <79131150+tomzcn123@users.noreply.github.com>
2023-04-23 01:06:05 -0400

    Update stocks_app.py

--- stocks_app.py
+++ stocks_app.py
@@ -20,10 +20,18 @@ def fetch_stock_data(stock_ticker, period='100d', interval='1d'):
 
 @st.cache
 def calculate_macd(data, window_fast=12, window_slow=26, window_sign=9, macd_ma_window=5):
+    data = data.copy()  # Create a copy of the data
     macd_indicator = MACD(data['Close'], window_slow=window_slow, window_fast=window_fast, window_sign=window_sign)
     data[f'MACD_{window_fast}_{window_slow}_{window_sign}'] = macd_indicator.macd()
     data[f'MACD_{window_fast}_{window_slow}_{window_sign}_MA_{macd_ma_window}'] = data[f'MACD_{window_fast}_{window_slow}_{window_sign}'].rolling(window=macd_ma_window).mean()
     return data
+
+@st.cache
+def calculate_moving_average(data, window=20):
+    data = data.copy()  # Create a copy of the data
+    data[f'MovingAverage_{window}'] = data['Close'].rolling(window=window).mean()
+    return data
+
 
 @st.cache(suppress_st_warning=True)
 def find_stocks_above_conditions(stock_list):
@@ -48,10 +56,6 @@ def find_stocks_above_conditions(stock_list):
     return stocks_above_conditions, error_messages
 
 
-@st.cache
-def calculate_moving_average(data, window=20):
-    data[f'MovingAverage_{window}'] = data['Close'].rolling(window=window).mean()
-    return data
 
 def plot_candlestick_chart(stock_ticker, period='3mo', interval='1d'):
     data = yf.download(tickers=stock_ticker, period=period, interval=interval)
