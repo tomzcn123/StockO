6f02f7d7b5f665ffd909c9b4d79014f8edc4097e
tomzcn123 <79131150+tomzcn123@users.noreply.github.com>
2023-04-22 20:54:29 -0400

    Update stocks_app.py

--- stocks_app.py
+++ stocks_app.py
@@ -34,33 +34,31 @@ def calculate_macd(stock_ticker, window_fast=12, window_slow=26, window_sign=9):
 def find_stocks_above_conditions(stock_list):
     stocks_above_conditions = defaultdict(list)
 
-    for stock_info in stock_list:
-        stock = stock_info['Symbol']
-        sector = stock_info['GICS Sector']
+    for stock in stock_list:
         try:
-            data = calculate_moving_average(stock)
-            data = calculate_macd(data, window_fast=5, macd_ma_window=5)
-            data = calculate_macd(data)
+            data = (calculate_moving_average(stock)
+                    .pipe(calculate_macd, window_fast=5)
+                    .pipe(calculate_macd))
 
             if (not data.empty and
                 data.iloc[-1]['Close'] > data.iloc[-1]['MovingAverage'] and
-                data.iloc[-1][f'MACD_5_26_9_MA_5'] > data.iloc[-1]['MACD_5_26_9']
+                data.iloc[-1]['Close'] > data.iloc[-1]['MACD_5_26_9']
             ):
+                sector = get_stock_sector(stock)
                 stocks_above_conditions[sector].append(stock)
+                print(f"Stock {stock} in sector {sector} meets the conditions.")  # Add this line
         except Exception as e:
             st.warning(f"Error processing stock {stock}: {e}")
 
     return stocks_above_conditions
 
 
+
 @st.cache
 def plot_candlestick_chart(stock_ticker, period='3mo', interval='1d', window_fast=12, window_slow=26, window_sign=9):
     data = yf.download(tickers=stock_ticker, period=period, interval=interval)
-
     macd_data_20 = calculate_macd(stock_ticker)
     macd_data_5 = calculate_macd(stock_ticker, window_fast=5, window_slow=20, window_sign=5)
-
-
     fig = go.Figure()
 
     fig.add_trace(go.Candlestick(x=data.index,
